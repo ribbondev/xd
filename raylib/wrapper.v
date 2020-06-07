634dabb18edf324520b194791c710e57aa7ba10e
lilah <lilah.willacott@gmail.com>
2020-06-07 09:50:45 +0100

    add more wrapper functions

--- raylib/wrapper.v
+++ raylib/wrapper.v
@@ -18,7 +18,38 @@ pub mut:
 }
 
 fn C.InitWindow(width, height int, title charptr)
+fn C.CloseWindow()
+fn C.SetTargetFPS(fps int)
+fn C.BeginDrawing()
+fn C.EndDrawing()
+fn C.WindowShouldClose() bool
 
+[inline]
 pub fn init_window(width, height int, title string) {
 	C.InitWindow(width, height, title.str)
+}
+
+[inline]
+pub fn set_target_fps(fps int) {
+ 	C.SetTargetFPS(fps)
+}
+
+[inline]
+pub fn begin_drawing() {
+	C.BeginDrawing()
+}
+
+[inline]
+pub fn end_drawing() {
+	C.EndDrawing()
+}
+
+[inline]
+pub fn window_should_close() bool {
+ 	return C.WindowShouldClose()
+}
+
+[inline]
+pub fn close_window() {
+	C.CloseWindow()
 }
--- xd.v
+++ xd.v
@@ -5,5 +5,12 @@ import raylib
 
 fn main() {
 	engine.test()
-	raylib.init_window(640, 480, "hello world")
+	raylib.init_window(640, 480, 'hello world')
+	raylib.set_target_fps(144)
+	for (!raylib.window_should_close()) {
+		raylib.begin_drawing()
+		// todo: draw
+		raylib.end_drawing()
+	}
+	raylib.close_window()
 }
