4dda4d67853332a49b566fad9977fc2c908bccd2
lilah <lilah.willacott@gmail.com>
2020-08-04 16:01:35 +0100

    move config to its own file

---
+++ src/config.d
@@ -0,0 +1,24 @@
+module config;
+
+import dyaml;
+
+struct Config {
+  Window window;
+  this(const Node node, string _) {
+    auto window_node = node["window"];
+    window.title = window_node["title"].as!string;
+    auto resolution_node = window_node["resolution"];
+    window.resolution.x = resolution_node["x"].as!int;
+    window.resolution.y = resolution_node["y"].as!int;
+  }
+}
+
+struct Window {
+  string title;
+  Resolution resolution;
+}
+
+struct Resolution {
+  int x;
+  int y;
+}
--- src/xd.d
+++ src/xd.d
@@ -5,27 +5,7 @@ import raylib.core;
 import raylib.text;
 import raylib.gui;
 import dyaml;
-
-struct Config {
- Window window;
- this(const Node node, string _) {
-   auto window_node = node["window"];
-   window.title = window_node["title"].as!string;
-   auto resolution_node = window_node["resolution"];
-   window.resolution.x = resolution_node["x"].as!int;
-   window.resolution.y = resolution_node["y"].as!int;
- }
-}
-
-struct Window {
-  string title;
-  Resolution resolution;
-}
-
-struct Resolution {
-  int x;
-  int y;
-}
+import config;
 
 void main() {
   const root = Loader.fromFile("xd.conf.yml").load();
